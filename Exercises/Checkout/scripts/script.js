8e57cae8cd702cc920ba5a2d9b0cc73ebb18c023
Dan Bejan <dbejan@tremend.ro>
2015-10-15 01:57:29 +0300

    Using fors and ifs to solve it. No functions here

--- Exercises/Checkout/scripts/script.js
+++ Exercises/Checkout/scripts/script.js
@@ -8,3 +8,50 @@
  */
 
 var final_prices = [];
+
+for (order in orders)
+{
+	var orderPrice = 0.0
+	var orderId = orders[order]["id"];
+	for (orderItem in orders[order]["items"])
+	{
+		var itemId = orders[order]["items"][orderItem]["item"];
+		var orderQty = orders[order]["items"][orderItem]["quantity"];
+		for (item in items)
+		{
+			if (itemId == items[item]["code"])
+			{
+				var itemCost = items[item]["cost"];
+				break
+			}
+		};
+		var orderItemCost = orderQty * itemCost;
+		orderPrice += orderItemCost;
+	};
+	if (typeof orders[order]["employee"] != "undefined")
+	{
+		for (employee in employees)
+		{
+			if (employees[employee]["id"] == orders[order]["employee"])
+			{
+				var employeeDiscount = employees[employee]["discount"];
+				break;
+			}
+		}
+
+		if (employeeDiscount > 70)
+		{
+			orderPrice = false
+		} else {
+			console.log(orderPrice);
+			orderPrice -= (orderPrice*employeeDiscount)/100;
+		}
+	}
+	finalOrder = {
+		order_id: orderId,
+		cost: orderPrice
+	};
+	final_prices[order] = finalOrder;
+	console.log(finalOrder);
+};
+console.log(final_prices);
